920b88c83dbc7124eefa319ede74449351886f4d
Sravan Chand <101858035+ogfourtwo@users.noreply.github.com>
2025-02-05 00:07:34 +0530

    Update dreamsetup.bat

--- dreamsetup.bat
+++ dreamsetup.bat
@@ -15,8 +15,8 @@ echo Installing required libraries...
 .\pip.exe install tqdm==4.67.1
 .\pip.exe install tomesd==0.1.2
 .\pip.exe install tensorboard==2.13.0
-.\pip.exe install torch==2.1.2+cu118 torchvision==0.16.2+cu118 --index-url https://download.pytorch.org/whl/cu118
-.\pip.exe install xformers==0.0.23.post1
+.\pip.exe install torch==2.1.2+cu121 torchvision==0.16.2+cu121 --index-url https://download.pytorch.org/whl/cu121
+.\pip.exe install xformers==0.0.21
 .\pip.exe install transformers==4.48.2
 
 echo Installation complete!
